4883114048d939a55eb58c4add6ba69eddd6dcae
John <marceljohn125@gmail.com>
2023-07-24 23:21:36 +0300

    reverted to node

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 8080; 
 
 //connect routes to their pages
-let index = fs.readFileSync('index.php');
+let index = fs.readFileSync('index.html');
 
 app.get('/', (req, res) => res.redirect('/index'));
 app.get('/index', (req, res) => res.end(index));
